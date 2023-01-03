46befea7ee4a028e3ec2fa3eb1523d9e076c948b
Vodhrin <78246218+Vodhrin@users.noreply.github.com>
2023-01-03 10:58:21 -0600

    caching attempt pt 2

--- ytdl.py
+++ ytdl.py
@@ -13,12 +13,12 @@ ytdl_format_options_audio = {
     'nocheckcertificate': True,
     'ignoreerrors': False,
     'logtostderr': False,
-    'quiet': True,
+    'verbose': True,
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
     'cookiefile': 'cookies.txt',
-    'rm_cachedir': True
+    'cachedir': False
 }
 ytdl_format_options_video = {
     'format': 'best',
@@ -28,12 +28,12 @@ ytdl_format_options_video = {
     'nocheckcertificate': True,
     'ignoreerrors': False,
     'logtostderr': False,
-    'quiet': True,
+    'verbose': True,
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
     'cookiefile': 'cookies.txt',
-    'rm_cachedir': True
+    'cachedir': False
 }
 
 
